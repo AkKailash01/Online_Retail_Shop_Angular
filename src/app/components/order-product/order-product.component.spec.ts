8fe443b1ad7fc1560f3f2f9512a2f1d4c522f24c
AkKailash01 <arunkkailash@gmail.com>
2025-01-29 17:34:29 +0530

    added more testcases

--- src/app/components/order-product/order-product.component.spec.ts
+++ src/app/components/order-product/order-product.component.spec.ts
@@ -3,7 +3,6 @@ import { HttpClientTestingModule } from '@angular/common/http/testing';
 import { OrderProductComponent } from './order-product.component';
 import { ProductService } from 'src/app/services/product.service';  
 import { ToastrModule } from 'ngx-toastr'; 
-import { of } from 'rxjs';
 describe('OrderProductComponent', () => {
   let component: OrderProductComponent;
   let fixture: ComponentFixture<OrderProductComponent>;
